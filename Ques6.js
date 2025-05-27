60aaa56dd61bacf5439d292647e9f5c910308965
Gauravkumar13045 <gauravkumar13045@gmail.com>
2025-05-28 00:01:52 +0530

    Question 6 done

---
+++ Ques6.js
@@ -0,0 +1,11 @@
+// 6. Write a function that returns the square of a number.
+
+function square(n){
+    let num = n * n ;
+    return num;
+}
+
+
+let sqrt = 5;
+
+console.log(`The square of ${sqrt} is ${square(sqrt)}`);
