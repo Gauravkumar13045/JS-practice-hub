758fbb29fae8499a26c65f21826e054b9615d957
Gauravkumar13045 <gauravkumar13045@gmail.com>
2025-05-28 14:45:14 +0530

    Question 9 done

--- Ques9.js
+++ Ques9.js
@@ -2,7 +2,26 @@
 
 
 function isPrime(num){
- 
+ let isprime = true;
+
+    if(num <= 1){
+        isprime = false;
+    }else if(num === 2){
+        isprime = true;
+    }else if( num % 2 === 0){
+        isprime = false;
+    }else{
+        for(let i = 3; i <= Math.sqrt(num); i += 2){
+            if(num % i === 0){
+                isprime = false;
+                break;
+            };
+        }
+    }
+   console.log(`${num} is ${isprime ? "a prime" : "not a prime"} number.`);
+    return isprime;
 
 }
 
+let num = 229;
+isPrime(num); 
