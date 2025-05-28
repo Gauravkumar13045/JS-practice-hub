93f88ddfdb1eec3b763ab84e508fa13d9542f6d3
Gauravkumar13045 <gauravkumar13045@gmail.com>
2025-05-28 15:34:46 +0530

    Question 11 done

---
+++ Ques11.js
@@ -0,0 +1,10 @@
+// 11. Write a program to print a multiplication table for a number.
+
+let n = 5;
+
+for(let i = 0; i <=10 ; i++){
+    let count = n * i;
+
+    console.log(`${n} * ${i} = ${count}`);
+    
+}
