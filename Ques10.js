07e2ae8db6c2ae0e2f6cce7b2ccaba38397ffed4
Gauravkumar13045 <gauravkumar13045@gmail.com>
2025-05-28 15:08:38 +0530

    Question 10 done

---
+++ Ques10.js
@@ -0,0 +1,21 @@
+// 10. Write a function that returns the sum of all numbers from 1 to n.
+
+function sumToN(n) {
+
+    let sum = 0;
+    if (n < 1) {
+        console.log("number is less than 1");
+        return 0;
+    } else 
+         {
+          for (let i = 1; i <= n; i++) {
+            sum += i;
+            
+        }
+     }
+     console.log(`Sum of numbers from 1 to ${n} is: ${sum}`);
+
+     return sum;
+ 
+}
+sumToN(5);
