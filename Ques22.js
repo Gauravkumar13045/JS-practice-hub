0072e4ae1e3ec44fe45e8328aadbdb7d9a10ff21
Gauravkumar13045 <gauravkumar13045@gmail.com>
2025-05-30 08:46:10 +0530

    Question 22 done

---
+++ Ques22.js
@@ -0,0 +1,10 @@
+// 22. Write a program that prints all even numbers between 1 and 50.
+
+
+let count = 50;
+
+for(let i = 1; i <= count; i++){
+    if(i % 2 === 0){
+        console.log(i);
+    }
+}
