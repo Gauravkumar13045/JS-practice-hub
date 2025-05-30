6a97ad8b34eea1dbe6954349bbac1bf13e3a0263
Gauravkumar13045 <gauravkumar13045@gmail.com>
2025-05-30 08:55:42 +0530

    Question 23 done

---
+++ Ques23.js
@@ -0,0 +1,10 @@
+// 23. Write a function that calculates the average of three numbers.
+
+function AvgNum(a,b,c){
+
+    let average = (a+b+c)/3;
+    console.log(`The average is: ${average}`);
+
+}
+
+AvgNum(4,5,6);
