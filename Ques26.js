89e1813aaccb9c4a339ca7202e2a28fc84b08b0a
Gauravkumar13045 <gauravkumar13045@gmail.com>
2025-05-30 13:32:06 +0530

    Question 26 done

---
+++ Ques26.js
@@ -0,0 +1,13 @@
+// 26. Create an array of 10 numbers and print only the even numbers.
+
+let num = [1,2,3,4,5,6,7,8,9,10];
+
+for(let i = 0; i < num.length; i++){
+    if(num[i] % 2 === 0){
+        console.log(num[i]);
+    
+
+    }
+}
+
+
