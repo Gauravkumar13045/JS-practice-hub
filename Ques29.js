8ce9d4fb08180d0b2dca56751ff7a2e780a9533b
Gauravkumar13045 <gauravkumar13045@gmail.com>
2025-06-12 16:08:58 +0530

    Question 29 done

---
+++ Ques29.js
@@ -0,0 +1,14 @@
+// 29. Create an object representing a person with properties: name, age, and city.
+
+const person = {
+    name: "Gaurav",
+    age: 20,
+    city: "Patna",
+};
+
+console.log(person.name);
+console.log(person.age);
+console.log(person.city);
+
+
+
