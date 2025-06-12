005345538c5e5cf28bb1ba6f72d199e00a09c870
Gauravkumar13045 <gauravkumar13045@gmail.com>
2025-06-12 16:51:26 +0530

    Question 32 done

---
+++ Ques32.js
@@ -0,0 +1,10 @@
+// 32. Write a function to merge two arrays into one.
+
+
+
+function joiner(array1, array2){
+let finalArray = array1.concat(array2);
+console.log(finalArray);
+}
+
+joiner([1,2,3,4,5], [6,7,8,9,0]);
