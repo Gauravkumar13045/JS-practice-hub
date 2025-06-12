e66350d09168c913f22260102f46c32cb78f5e61
Gauravkumar13045 <gauravkumar13045@gmail.com>
2025-06-12 16:24:57 +0530

    Question 30 done

---
+++ Ques30.js
@@ -0,0 +1,9 @@
+// 5. Write a function to count the number of properties in an object.
+
+const person = {
+    name: "Gaurav",
+    age: 20,
+    city: "Patna",  
+};
+
+console.log(Object.keys(person).length)
