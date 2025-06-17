23372157c9e57023955bd47e29f2f9a145e3370c
Gauravkumar13045 <gauravkumar13045@gmail.com>
2025-06-17 11:25:42 +0530

    Ques34 completed

--- Ques34.js
+++ Ques34.js
@@ -1,5 +1,9 @@
 // 9. Write a function to reverse an array without using .reverse().
 
-function reversearray(){
-    
+function reversearray(arr) {
+
+    return arr.reduce((acc, curr) => [curr, ...acc], []);
+
 }
+
+console.log(reversearray([1, 2, 3, 4, 5])); 
